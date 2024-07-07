85a191c80e996c3dc93e90906d9c6c2996f084c6
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2024-07-07 19:27:42 +0300

    -add get started button widgets -add style of button

--- lib/feature/on_boarding/view/screens/on_boarding.dart
+++ lib/feature/on_boarding/view/screens/on_boarding.dart
@@ -1,7 +1,9 @@
+import 'package:doc_doc/core/theming/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
+import '../widgets/doctot_logo_with_opacity_and_text.dart';
+import '../widgets/get_started_button.dart';
 import '../widgets/logo_and_name.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -10,20 +12,32 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 30.h),
-      child: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 30.h),
-            const LogoAndName(),
-            SizedBox(height: 30.h),
-            Stack(
-              children: [
-                SvgPicture.asset('assets/svgs/docdoc_logo_low_opacity.svg')
-              ],
-            )
-          ],
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.w,vertical: 30.h),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 30.h),
+              const LogoAndName(),
+              SizedBox(height: 30.h),
+              const DoctorLogoWithOpacityAndText(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25.h),
+                child: Column(
+                  children: [
+                    Text(
+                      'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                      textAlign: TextAlign.center,
+                      style: Styles.font13GrayRegular,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30.h),
+              const GetStartedButton()
+            ],
+          ),
         ),
       ),
     ));
