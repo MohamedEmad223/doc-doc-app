4893364d0a765636d0db26f28752706cc697d3a7
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2024-07-07 18:24:46 +0300

    -add doc doc logo with low opacity to assets file -add logo and name widget add style file to helpers folders

---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Styles{
+  Styles._();
+  static TextStyle font24Black700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+}
--- lib/feature/on_boarding/view/screens/on_boarding.dart
+++ lib/feature/on_boarding/view/screens/on_boarding.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widgets/logo_and_name.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Onboarding Screen'),
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 30.h),
+      child: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 30.h),
+            const LogoAndName(),
+            SizedBox(height: 30.h),
+            Stack(
+              children: [
+                SvgPicture.asset('assets/svgs/docdoc_logo_low_opacity.svg')
+              ],
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
