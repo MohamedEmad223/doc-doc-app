c63e928e8ca5946149c36e1b023ee7749407045a
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2024-07-07 09:27:45 +0300

    setup project

---
+++ lib/doc_app.dart
@@ -0,0 +1,26 @@
+
+import 'package:doc_doc/core/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routes/routs.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key,required this.appRouter});
+ final AppRoutes appRouter;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  ScreenUtilInit(
+        designSize:  const Size(375, 812),
+        minTextAdapt: true,
+        child: MaterialApp(
+          title: 'Doc App',
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.onBoardingScreen,
+          onGenerateRoute: appRouter.onGenerateRoute,
+        )
+    );
+  }
+}
